de208272f6a02794fe1eaf85875b26ca9dfb64c2
ardiansyahp8 <ardiansyahp8@gmail.com>
2022-03-27 22:57:15 +0700

    Updating Data.js and looping it to create components for every tracks listed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
-import Track from "./components/Track";
-import data from './Data';
+import Button from "../UI/Button";
+import Image from "../UI/Image";
 
-function App() {
+const Track = ({ item }) => {
   return (
     <div className="App">
-      <Track data={data} />
+      <div className='music-content'>
+        <Image
+        title={item.name}
+        imgUrl={item.album.images[0].url}
+        width="275px"
+        height="275px"
+        />
+        <h2>{item.name</h2>
+        <h3>{item.album.name}</h3>
+        <p>{item.artists[0].name}</p>
+        <div>
+          <Button text="Select" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Track;
